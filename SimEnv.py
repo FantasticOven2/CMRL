92266a76133875cf267ff60cadfe9a07e196d7f9
FantasticOven2 <0z5e1n3g@berkeley.edu>
2022-06-05 01:18:58 -0700

    one hot

---
+++ SimEnv.py
@@ -0,0 +1,161 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+# from mushroom_rl.algorithms.actor_critic import PPO, SAC, TD3
+# from mushroom_rl.core import Environment, MDPInfo
+# from mushroom_rl.utils import spaces
+from mushroom_rl.utils.viewer import Viewer
+
+import gym
+from gym import Env
+from gym.spaces import Discrete, Box
+
+class SimEnv(Env):
+  
+    def __init__(self, time_step = 0.1, eps = 0.5):
+        self.time_step = time_step
+
+        self._viewer = Viewer(env_width = 22, env_height = 22, background = (0, 0, 0))
+        
+        self.action_space = Box(np.array([-1, -1]), np.array([1, 1]))
+        self.observation_space = Box(np.array([-10, -10, 0, 0]), np.array([10, 10, 1, 1]))
+        # mdp_info = MDPInfo(observation_space, action_space, gamma, horizon)
+        # super().__init__(mdp_info)
+
+        self.state = np.array([0.0, 0.0, 1.0, 0.0])
+        # self.state = np.array([10.0, 0.0, 1.0, 0.0])
+        self.intersection = np.array([10.0, 0.0, 1.0, 0.0])
+        self.goal = np.array([10.0, 10.0, 0.0, 1.0])
+
+        self.eps = eps
+        # self.next_manifold = False
+
+
+    def _sparseReward(self, state):
+        done = False
+        if np.array_equal(state, self.goal):
+            reward = 100
+            done = True
+        elif np.array_equal(state, self.intersection) and self.state[3] == 0:
+            reward = 10
+        else:
+            reward = 0
+        return reward, done
+        
+
+    def check_constraints(self):
+        return ((self.state[1] < -self.eps) + (self.state[1] > self.eps)) * (
+            (self.state[0] < 10 - self.eps) + (self.state[0] > 10 + self.eps)) 
+
+    def _reward(self, state):
+        done = False
+
+        ### Fall off the manifold ###
+        if self.check_constraints():
+            # Negative of l2 distance to current manifold
+            # if self.state[2] == 1.0:
+            #     reward = -np.abs(state_1 - 10)
+            # else:
+            #     reward = -np.abs(state_2)
+            reward = -100
+            done = True
+        
+        ### Reach the interseciton ###
+        elif np.array_equal(state, self.intersection):
+            if self.state[3] == 1.0:
+                reward = -np.abs(self.state[1] - 10)
+            else:
+                reward = 10
+
+        ### Reach the goal ###
+        elif np.array_equal(state, self.goal):
+            reward = 100
+            done = True
+
+        ### Direct the agent to intersection point / goal
+        else: 
+            if self.state[3] == 1.0:
+                reward = -np.abs(self.state[1] - 10)
+                print('second manfold: ', reward)
+            else: 
+                reward = - np.abs(self.state[0] - 10)
+                print('first manifold: ', reward)
+        return reward, done
+
+    def step(self, action):
+        ### Projection ###
+        if self.state[3] == 1.0: 
+            _action = np.array([0.0, action[1]])
+            # _action = np.array([[0.0, 0.0], [0.0, 1.0]]) @ _action
+        else:
+            _action = np.array([action[0], 0.0])
+            # _action = np.array([[1.0, 0.0], [0.0, 0.0]]) @ _action
+
+        self.state[0] += _action[0]
+        self.state[1] += _action[1]
+        
+        reward, done = self._reward(self.state)
+        # reward, done = self._sparseReward(self.state)
+
+        if self.state[0] == 10.0 and self.state[1] == 0.0:
+            self.state[2] = 0.0
+            self.state[3] = 1.0
+
+        info = {}
+        return self.state, reward, done, info
+
+    def render(self):
+        start = np.array([10.0, 10.0])
+        intersection = np.array([20.0, 10.0])
+        goal = np.array([20.0, 20.0])
+        agent = np.array([10.0, 10.0])
+
+        hline_start = np.array([0.0, 10])
+        hline_end = np.array([22, 10])
+        vline_start = np.array([20, 0])
+        vline_end = np.array([20, 22])
+        self._viewer.line(hline_start, hline_end, color = (100, 100, 100), width = 5)
+        self._viewer.line(vline_start, vline_end, color = (100, 100, 100), width = 5)
+        self._viewer.circle(center = start, radius = 0.3, color = (255, 0, 0))
+        self._viewer.circle(center = intersection, radius = 0.3, color = (0, 0, 255))
+        self._viewer.circle(center = goal, radius = 0.3, color = (0, 255, 0))
+        self._viewer.circle(center = agent + self.state[:2], radius = 0.3, color = (50, 100, 150))
+        self._viewer.display(self.time_step)
+        self._viewer.close()
+
+    def reset(self, eval=False, prob=0.4):
+        if not eval:
+            rand = np.random.uniform()
+            if rand < prob:
+                print('1')
+                self.state = np.array([0.0, 0.0, 1.0, 0.0])
+            else:
+                print('2')
+                self.state = np.array([10.0, 0.0, 1.0, 0.0])
+        # self.next_manifold = False
+        else: 
+            self.state = np.array([0.0, 0.0, 1.0, 0.0])
+        return self.state
+
+if __name__ == '__main__':
+    env = SimEnv()
+    env.state = np.array([1, 0.4, 1, 0])
+    print(env.check_constraints())
+    env.state = np.array([0.0, 0.0, 1.0, 0.0])
+    print(env.check_constraints())
+    env.state = np.array([1, -0.4, 1, 0])
+    print(env.check_constraints())
+    env.state = np.array([0, 0, 1, 0])
+    print(env.check_constraints())
+    env.state = np.array([10, 0, 1, 0])
+    print(env.check_constraints())
+    env.state = np.array([10.1, 2, 1, 0])
+    print(env.check_constraints())
+    env.state = np.array([9.9, 2, 1, 0])
+    print(env.check_constraints())
+
+    # for _ in range(10):
+    #     env.state[0] += 1
+    #     env.render() 
---
+++ experiment.py
@@ -0,0 +1,80 @@
+# import os
+# import argparse
+# from copy import deepcopy
+# from tqdm import trange
+# import pandas as pd
+
+# from mushroom_rl.algorithms.actor_critic import PPO, TRPO, DDPG, TD3, SAC
+# from mushroom_rl.core import Core, Logger
+# from mushroom_rl.policy import GaussianTorchPolicy, OrnsteinUhlenbeckPolicy, ClippedGaussianPolicy
+# from mushroom_rl.utils.dataset import compute_J, parse_dataset
+# from atacom.environments.circular_motion import CircleEnvAtacom, CircleEnvTerminated, CircleEnvErrorCorrection
+# from network import *
+
+# from SimEnv import SimEnv
+
+# def build_agent_PPO(mdp_info, actor_lr, critic_lr, n_features, batch_size, eps_ppo, 
+#                     lam, ent_coeff, **kwargs):
+#     policy_params = dict(
+#         std_0 = 0.5,
+#         n_features = n_features,
+#         use_cuda = torch.cuda.is_available()
+#     )
+
+#     policy = GaussianTorchPolicy(PPONetwork,
+#                                 mdp_info.observation_space.shape,
+#                                 mdp_info.action_space.shape,
+#                                 **policy_params)
+
+#     critic_params = dict(network = PPONetwork,
+#                         optimizer = {'class': optim.Adam,
+#                                     'params': {'lr': critic_lr}},
+#                         loss = F.mse_loss,
+#                         n_features = n_features,
+#                         batch_size = batch_size,
+#                         input_shape = mdp_info.observation_space.shape,
+#                         output_shape = (1,))
+    
+#     ppo_params = dict(actor_optimizer={'class': optim.Adam,
+#                                        'params': {'lr': actor_lr}},
+#                       n_epochs_policy=4,
+#                       batch_size=batch_size,
+#                       eps_ppo=eps_ppo,
+#                       lam=lam,
+#                       ent_coeff=ent_coeff,
+#                       critic_params=critic_params)    
+    
+#     build_params = dict(compute_entropy_with_states=False,
+#                         compute_policy_entropy=True)
+
+#     return PPO(mdp_info, policy, **ppo_params), build_params
+    
+# if __name__ == '__main__':
+#     env = SimEnv()
+#     agent, build_params = build_agent_PPO(mdp_info = env.info)
+
+import os
+import random
+import numpy as np
+import torch
+from stable_baselines3 import SAC, PPO, TD3
+from SimEnv import SimEnv
+
+env = SimEnv(time_step = 0.5)
+device = torch.device(
+        "cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+model = PPO('MlpPolicy', env, verbose=1, device=device, ent_coef=0.15)
+print('model: ', model)
+model = model.learn(total_timesteps=100000, eval_freq=1000)
+
+# Evaulation
+obs = env.reset(eval=True)
+
+for _ in range(50):
+    action, _states = model.predict(obs, deterministic=True)
+    print('======')
+    print('action: ', action)
+    obs, rewards, dones, info = env.step(action)
+    print('obs: ', obs)
+    print('reward: ', rewards)
+    env.render()
